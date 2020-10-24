8baf90c091b4a6185fe25b97787226a2679d2e1d
max <me@mclifford.dev>
2020-10-24 19:25:26 +0100

    Updated routes

--- FBlob.Host/WebHost/DAL.fs
+++ FBlob.Host/WebHost/DAL.fs
@@ -1,2 +1,17 @@
 module FBlob.Host.WebHost.DAL
 
+
+
+module Content =
+    
+    let i = 0
+    
+    
+module Routes =
+    
+    let i = 0
+    
+    
+module Settings =
+    
+    let i = 0
--- FBlob.Host/WebHost/Routing.fs
+++ FBlob.Host/WebHost/Routing.fs
@@ -1,63 +1,78 @@
 module FBlob.Host.WebHost.Routing
 
+open System
 open System.IO
 open Http.ContentTypes
-open Http
 
 type Route =
-    { paths: string seq
-      contentType: ContentType
-      routeType: RouteType
-      content: byte array option }
+    { Paths: string seq
+      ContentType: ContentType
+      Handler: (Http.Request -> byte array) }
 
-and RouteType = | Static
-//        | Dynamic
-//
-//    and DynamicHandler =
-//        { name: string }
-//        member this.Handler(request: Request) = [|0uy|]
+and StaticRouteType = { Name: string }
+
+and FileRouteType = { Path: string; ContentType: ContentType; }
+
+and BlobRouteType = { Reference: Guid }
+
+and ActionRouteType = { Action: string; ContentType: ContentType }
+
+and RouteType =
+    | Static of StaticRouteType
+    | File of FileRouteType
+    | Blob of BlobRouteType
+    | Action of ActionRouteType
 
 and RouteSetting =
-    { contentType: ContentType
-      routePaths: string seq
-      contentPath: string
-      routeType: RouteType }
-
-and HandlerType = | Static
+    { RoutePaths: string seq
+      RouteType: RouteType }
 
 and ErrorRoutes =
-    { badRequest: Route
-      notFound: Route
-      unauthorized: Route
-      internalError: Route }
+    { BadRequest: Route
+      NotFound: Route
+      Unauthorized: Route
+      InternalError: Route }
+
+let createStaticRoute settings srtSettings = ()
+    
+    // TODO fetch content type of static item in store.
+    
+    //
+    //Ok {
+    //    Paths = settings.RoutePaths
+    //    ContentType = 
+    //}
+
 
 /// Create a static route.
 /// Because the content is static it is
 /// loaded into memory when the route is created.
-let createStaticRoute settings =
+let createFileRoute settings frtSettings  =
 
-    match File.Exists(settings.contentPath) with
+    match File.Exists(frtSettings.Path) with
     | true ->
-        let data = File.ReadAllBytes(settings.contentPath)
+        let data = File.ReadAllBytes(frtSettings.Path)
         Ok
-            { paths = settings.routePaths
-              contentType = settings.contentType
-              routeType = settings.routeType
-              content = Some data }
-    | false ->
-        let message =
-            sprintf "Could not load static content: '%s'." settings.contentPath
+            { Paths = settings.RoutePaths
+              ContentType = frtSettings.ContentType
+              Handler = fun _ -> data }
+    | false -> Error (sprintf "Could not load static content: '%s'." frtSettings.Path)
 
-        Error message
+let tryCreateRoute settings =
+    match settings.RouteType with
+    | Static srt -> Error "Not implemented"
+    | File frt -> createFileRoute settings frt
+    | Blob brt -> Error "Not implemented"
+    | Action art -> Error "Not implemented"
 
 let createRoutes<'a, 'b> results =
     results
-    |> Seq.map createStaticRoute
+    |> Seq.map tryCreateRoute
     |> FUtil.Results.splitResults
 
 let createRouteMap (state: Map<string, Route>) (route: Route) =
     let newRoutes =
-        route.paths
+        route.Paths
         |> Seq.map (fun r -> (r, route))
         |> Map.ofSeq
 
@@ -73,5 +88,3 @@ let matchRoute (routes: Map<string, Route>) (notFound: Route) (route: string) =
     match routes.TryFind route with
     | Some route -> route
     | None -> notFound
-    
-
